d30484bbad1346f2fe6cb254b41b697f62bfbb2f
Kornel Lugosi <coornail@gmail.com>
2014-09-16 21:15:26 +0200

    Implement configurable house edge

--- martingale.R
+++ martingale.R
@@ -1,15 +1,16 @@
-martingaleRun <- function(startMoney, baseBet = 1, betIncrease = 2.0) {
+martingaleRun <- function(startMoney, baseBet = 1, betIncrease = 2.0, houseEdge=0.01) {
   mVector <- c();
   money <- startMoney;
   bet <- baseBet;
+  bettingAgainst <- (0.5-(houseEdge/2));
   maxMoney <- startMoney;
   maxMoneyAtStep <- 0;
   step <- 0;
-
+  
   while (bet < money) {
     step <- step + 1;
-    mVector[step] <- money;
-    if (runif(1) < 0.495) {
+    #mVector[step] <- money;
+    if (runif(1) < bettingAgainst) {
       money <- money + bet;
       bet <- baseBet;
     } else {
